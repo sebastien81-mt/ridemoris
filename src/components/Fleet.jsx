79e28ae725fc80f9f0d3314b08f1f69dc5e07306
sebastien81-mt <sebastien.bluewindow@gmail.com>
2026-08-16 00:49:09 +0200

    Add commercial studio photograph in AVIF format for Suzuki Brezza

--- src/components/Fleet.jsx
+++ src/components/Fleet.jsx
@@ -175,7 +175,7 @@ const FLEET_DATA = [
     transmission: 'AUTOMATIC',
     seats: 5,
     luggage: '3 Bags',
-    image: 'https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?auto=format&fit=crop&w=800&q=80'
+    image: '/images/suzuki-brezza.avif'
   },
   {
     id: 'magnite',
