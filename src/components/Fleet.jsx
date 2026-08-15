ba5bcc05427dd9c5e10ee84ccda767649a3ceccf
sebastien81-mt <sebastien.bluewindow@gmail.com>
2026-08-15 23:12:29 +0200

    Add commercial studio photograph for Honda Jazz

--- src/components/Fleet.jsx
+++ src/components/Fleet.jsx
@@ -113,7 +113,7 @@ const FLEET_DATA = [
     transmission: 'AUTOMATIC',
     seats: 5,
     luggage: '3 Bags',
-    image: 'https://images.unsplash.com/photo-1552519507-da3b142c6e3d?auto=format&fit=crop&w=800&q=80'
+    image: '/images/honda-jazz.jpg'
   },
   {
     id: 'mirage',
