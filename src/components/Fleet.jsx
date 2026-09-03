95625d169dfab36cfd07d6eadca8f3fa58dec7ad
sebastien81-mt <sebastien.bluewindow@gmail.com>
2026-09-03 23:18:37 +0200

    Add commercial studio photograph in AVIF format for Mitsubishi Outlander Sport

--- src/components/Fleet.jsx
+++ src/components/Fleet.jsx
@@ -245,7 +245,7 @@ const FLEET_DATA = [
     transmission: 'AUTOMATIC',
     seats: 5,
     luggage: '4 Bags',
-    image: 'https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?auto=format&fit=crop&w=800&q=80'
+    image: '/images/mitsubishi-outlander-sport.avif'
   },
   {
     id: 'vezel',
